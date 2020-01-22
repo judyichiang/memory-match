f243570c2737ceb485c708fc116ea9868338a8cb
Judy Chiang <jiyc1119@gmail.com>
2020-01-22 12:49:12 -0800

    cards showing on the board

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,11 @@ var mainElem = document.getElementById('gameCards');
 
 mainElem.addEventListener("click", handleClick);
 
-function handleClick(element){
-  console.log(element.target);
+function handleClick(element) {
+  // console.log(element.target);
+  if (event.target.className.indexOf("card-back") === -1) {
+    return;
+  }
   element.target.className = 'hidden'
 
 }
