4a69fd763b97440bac85971c957c7392590cf2c8
Judy Chiang <jiyc1119@gmail.com>
2020-01-22 12:38:35 -0800

    changed bootstrap css and added hidden

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
+var mainElem = document.getElementById('gameCards');
 
+mainElem.addEventListener("click", handleClick);
+
+function handleClick(element){
+  console.log(element.target);
+  element.target.className = 'hidden'
+
+}
